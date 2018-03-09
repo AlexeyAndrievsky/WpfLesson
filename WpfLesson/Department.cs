6e165d52c77cf20915e83108f7f404d5b6b72215
Alexey Andrievsky <andrievskyalexey@gmail.com>
2018-03-09 18:08:52 +0300

    First commit

---
+++ WpfLesson/Department.cs
@@ -0,0 +1,19 @@
+﻿namespace WpfLesson
+{
+    public class Department
+    {
+        //TODO: Доступные для записи из вне поля - никуда не годится. Позже переделаю.
+        public int DepartmentNumber { get; set; }
+        public string DepartmentName { get; set; }
+        public string Info { get; set; }
+
+        public Department(int departmentNumber, string departmentName, string info)
+        {
+            DepartmentNumber = departmentNumber;
+            DepartmentName = departmentName;
+            Info = info;
+        }
+    }
+
+
+}
---
+++ WpfLesson/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WpfLesson
+{
+    public class Employee
+    {
+        //TODO: Доступные для записи из вне поля - никуда не годится. Позже переделаю.
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Passport { get; set; }
+        public string Addres { get; set; }
+        public int DepartmentNumber { get; set; }
+
+        public Employee(string surname, string name, string secondName, DateTime birthday, string passport, string addres, int departmentNumber)
+        {
+            Name = name;
+            Surname = surname;
+            SecondName = secondName;
+            Birthday = birthday;
+            Passport = passport;
+            Addres = addres;
+            DepartmentNumber = departmentNumber;
+        }
+    }
+
+    
+}
