115396a28948d35c1a1cd981a675a57c2eb3f5f8
Andrievsky Alexey <andrievskyalexey@gmail.com>
2018-03-16 19:57:06 +0300

    UpdateCommand documentation added

--- WpfLesson/WpfLesson.ViewModel/UpdateCommand.cs
+++ WpfLesson/WpfLesson.ViewModel/UpdateCommand.cs
@@ -1,25 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace WpfLesson.ViewModel
 {
+    /// <summary>
+    /// Класс, реализующий интерфейс <see cref="ICommand"/>
+    /// служащий для обработки нажатия на кнопку обновлении экземпляра сущности.
+    /// </summary>
     internal class UpdateCommand : ICommand
     {
+        #region Const
+        /// <summary>
+        /// Константа для определения состояния равенства значений.
+        /// </summary>
         private const int ARE_EQUAL = 0;
-        private const int NONE_SELECTED = -1;
-        private IEntitiesViewModel vm;
 
+        /// <summary>
+        /// Константа для определения состояния "ничего не выбрано".
+        /// </summary>
+        private const int NONE_SELECTED = -1;
+        #endregion
+
+        #region Fields
+        /// <summary>
+        /// Ссылка на модель-представление.
+        /// </summary>
+        private IEntitiesViewModel vm;
+        #endregion
+
+        #region .ctor
+        /// <summary>
+        /// Конструктор класса UpdateCommand.
+        /// </summary>
+        /// <param name="viewModel">Ссылка на модель-представление.</param>
         public UpdateCommand(IEntitiesViewModel viewModel)
         {
             vm = viewModel;
             vm.PropertyChanged += vm_PropertyChanged;
         }
+        #endregion
 
+        #region Methods
+        /// <summary>
+        /// Обработчик события изменения значения свойства.
+        /// </summary>
         private void vm_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
             if (string.Compare(e.PropertyName, vm.SelectedPropertyName) == ARE_EQUAL)
@@ -28,6 +53,10 @@ namespace WpfLesson.ViewModel
             }
         }
 
+        /// <summary>
+        /// Метод для реализации интерфейса <see cref="ICommand"/>
+        /// определяющий возможность выполнения команды UpdateCommand
+        /// </summary>
         public bool CanExecute(object parameter)
         {
             if (vm.SelectedEntity == null)
@@ -35,11 +64,19 @@ namespace WpfLesson.ViewModel
             return ((EntityViewModel)vm.SelectedEntity).ID > NONE_SELECTED;
         }
 
+        /// <summary>
+        /// Событие для реализации интерфейса <see cref="ICommand"/>.
+        /// </summary>
         public event EventHandler CanExecuteChanged = delegate { };
 
+        /// <summary>
+        /// Метод для реализации интерфейса <see cref="ICommand"/>.
+        /// Инициализация вызова метода UpdateEntity в модель-представлении.
+        /// </summary>
         public void Execute(object parameter)
         {
             vm.UpdateEntity();
         }
+        #endregion
     }
 }
